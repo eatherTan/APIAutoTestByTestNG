855fa99dc47c8b1c88448ca94eab30976bb3aab4
xiaolian.tan <eather_tan@163.com>
2021-01-29 22:40:07 +0800

    自动生成实体

--- src/main/java/com/example/entity/UserInfoDomain.java
+++ src/main/java/com/example/entity/UserInfoDomain.java
@@ -1,4 +1,41 @@
 package com.example.entity;
 
 public class UserInfoDomain {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private Integer gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
 }
