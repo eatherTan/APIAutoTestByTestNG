d4971444f5a690acf3a3b3463f91e93326c75057
xiaolian.tan <eather_tan@163.com>
2021-01-29 19:20:05 +0800

    初始化项目 & 自动生成domain实体文件

---
+++ src/main/java/com/example/entity/UserInfoDomain1.java
@@ -0,0 +1,62 @@
+package com.example.entity;
+
+import java.io.Serializable;
+
+public class UserInfoDomain1 implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer gender;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append(", gender=").append(gender);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
